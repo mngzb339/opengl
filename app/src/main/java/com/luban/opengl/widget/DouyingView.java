89833392b9f7b41663cef80fb1cc9e0c16837134
liuyang <liuyang.liu@ximalaya.com>
2020-10-09 22:16:48 +0800

    commit:opengl 项目创建

---
+++ app/src/main/java/com/luban/opengl/widget/DouyingView.java
@@ -0,0 +1,20 @@
+package com.luban.opengl.widget;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * @author liuyang
+ */
+public class DouyingView extends GLSurfaceView {
+
+
+    public DouyingView(Context context) {
+        super(context);
+    }
+
+    public DouyingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
