c03f38b4d26657da98ab92f974f5065bd2f21cc7
liuyang <liuyang.liu@ximalaya.com>
2020-10-09 22:30:13 +0800

    commit:opengl配置部分代码提交

--- app/src/main/java/com/luban/opengl/MainActivity.java
+++ app/src/main/java/com/luban/opengl/MainActivity.java
@@ -1,13 +1,10 @@
 package com.luban.opengl;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
@@ -17,9 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
--- app/src/main/java/com/luban/opengl/widget/DouyingView.java
+++ app/src/main/java/com/luban/opengl/widget/DouyingView.java
@@ -11,10 +11,20 @@ public class DouyingView extends GLSurfaceView {
 
 
     public DouyingView(Context context) {
-        super(context);
+        super(context,null);
     }
 
     public DouyingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        /****************配置开始*******************/
+        //配置glSurfaceView
+        //1.设置egl 版本
+        setEGLContextClientVersion(2);
+        //2.设置一个渲染器
+        setRenderer(new DouyingRender());
+        //3.设置渲染器模式 设置按需渲染（当调用requestRender 请求GLThread 回调drawFrams
+        //连续渲染 自动处理
+        setRenderMode(RENDERMODE_WHEN_DIRTY);
+        /****************配置结束*******************/
     }
 }
